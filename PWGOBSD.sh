4b71cad33b9cfd658206b3c436c0b60b4dd7190e
Alexander Vereeken <37543796+Alexander88207@users.noreply.github.com>
2019-07-23 17:40:53 +0200

    Update PWGOBSD.sh

--- PWGOBSD.sh
+++ PWGOBSD.sh
@@ -6,10 +6,37 @@ BACKTITLE="PWGOBSD"
 TITLE="Welcome $USER :)"
 MENU="What do you want to do?:"
 
+OPTIONS=(1 "Installation"
+         2 "Start")
+         
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+cd /home/$USER
+mkdir /home/$USER/Games
+mkdir /home/$USER/Programs
+case $CHOICE in
+
+        1)
+#!/bin/bash
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="PWGOBSD"
+TITLE="Welcome $USER :)"
+MENU="What do you want to do?:"
+
 OPTIONS=(1 "Install Steam"
          2 "Install Blizzard"
          3 "Install Origin"
-         4 "Install Uplay")
+         4 "Install Uplay"
+         5 "Install Teamspeak")
 
 CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
@@ -22,6 +49,7 @@ CHOICE=$(dialog --clear \
 clear
 cd /home/$USER
 mkdir /home/$USER/Games
+mkdir /home/$USER/Programs
 case $CHOICE in
 
         1)
@@ -48,5 +76,15 @@ mkdir /home/$USER/Games/Uplay
 WINEPREFIX=/home/$USER/Games/Uplay winetricks corefonts # Fixes the blank window at start.       
 curl -O https://ubistatic3-a.akamaihd.net/orbit/launcher_installer/UplayInstaller.exe
 WINEPREFIX=/home/$USER/Games/Uplay wine /home/$USER/UplayInstaller.exe
-
+            ;;
+        5)
+mkdir /home/$USER/Programs/Teamspeak
+cd /home/$USER/Programs/Teamspeak
+curl -O https://oc.magzu.net/owncloud/index.php/s/C5BmsWkNiedJNzV/download
+unzip download
+rm download
+esac
+            ;;
+        2)
+echo WIP
 esac
