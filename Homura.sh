22b76402b7d06260fd0bdb9fe269afd0f0269fad
Alexander Vereeken <37543796+Alexander88207@users.noreply.github.com>
2019-09-06 23:47:42 +0200

    Update Homura.sh

--- Homura.sh
+++ Homura.sh
@@ -463,6 +463,7 @@ fi
 
 if [[ $UPDE == *"Homura to the latest version"* ]]; then
 cd /home/$USER/.local/share/$NAME
+rm VER
 curl -O https://raw.githubusercontent.com/Alexander88207/Homura/master/VER
 CHKVER=$(cat VER)
 if [[ $CHKVER == *"$VER"* ]]; then
@@ -475,7 +476,7 @@ tar -xf $NEXVER.tar.gz
 rm /home/$USER/.local/share/$NAME/$NAME.sh
 mv /home/$USER/.local/share/$NAME/$NAME-$NEXVER/$NAME.sh /home/$USER/.local/share/$NAME/$NAME.sh
 rm /home/$USER/$NEXVER.tar.gz
-rm -d -r /home/$USER/$NAME-$NEXVER
+rm -d -r /home/alexander/.local/share/$NAME/$NAME-$NEXVER
 fi
 fi
 fi
