b9fef8835154faed71d13747287795136291e0ac
Alexander Vereeken <37543796+Alexander88207@users.noreply.github.com>
2019-09-09 19:28:13 +0200

    Update Homura.sh

--- Homura.sh
+++ Homura.sh
@@ -124,10 +124,10 @@ echo $'\033]30;Homura 1.8 - Installation of itch\007'
 mkdir -p /home/$USER/.local/share/$NAME/Games/$INST
 cd /home/$USER/.local/share/$NAME/Games/$INST
 echo -e "\e[40;38;5;82mDownloading $INST\e[30;48;5;82m\e[0m"
-curl -o "itch-setup.exe" "http://alt.magzu.net/damn/dl/itch-setup.exe" 2>&1 | stdbuf -oL tr '\r' '\n' | sed -u 's/^ *\([0-9][0-9]*\).*\( [0-9].*$\)/\1\\:\2/' | zenity --progress --auto-close --text "Downloading $INST" --title "$NAME $VER $TODO $INST"
+curl -L -o "windows" "http://nuts.itch.zone/download/windows" 2>&1 | stdbuf -oL tr '\r' '\n' | sed -u 's/^ *\([0-9][0-9]*\).*\( [0-9].*$\)/\1\\:\2/' | zenity --progress --text "Downloading $INST" --title "$NAME $VER $TODO $INST"
 echo -e "\e[40;38;5;82mStarting installer\e[30;48;5;82m\e[0m"
-WINEPREFIX=/home/$USER/.local/share/$NAME/Games/$INST wine /home/$USER/.local/share/$NAME/Games/$INST/itch-setup.exe
-rm /home/$USER/.local/share/$NAME/Games/$INST/itch-setup.exe
+WINEPREFIX=/home/$USER/.local/share/$NAME/Games/$INST wine /home/$USER/.local/share/$NAME/Games/$INST/windows
+rm /home/$USER/.local/share/$NAME/Games/$INST/windows
 fi
 
 if [[ $INST == *"GOG Galaxy"* ]]; then
