2b41fec4cf031e8fa38f85853a4970c090bdde28
Zak Zajac <zak@madzak.com>
2019-09-10 08:14:34 -0700

    Removed hardcoded username

--- Setup.sh
+++ Setup.sh
@@ -13,7 +13,7 @@ tar -xf $VER.tar.gz
 mv /home/$USER/.local/share/$NAME/$NAME-$VER/$NAME.sh /home/$USER/.local/share/$NAME/$NAME.sh
 rm /home/$USER/.local/share/$NAME/$VER.tar.gz
 rm -d -r /home/$USER/.local/share/$NAME/$NAME-$VER
-mkdir -p /home/alexander/.local/share/applications
+mkdir -p /home/$USER/.local/share/applications
 echo "[Desktop Entry]
 Comment=
 Exec=bash /home/$USER/.local/share/$NAME/$NAME.sh
@@ -28,7 +28,7 @@ X-DBUS-ServiceName=
 X-DBUS-StartupType=
 X-KDE-SubstituteUID=false
 X-KDE-Username=
-" >> /home/alexander/.local/share/applications/$NAME.desktop
+" >> /home/$USER/.local/share/applications/$NAME.desktop
 fi
 
 if [[ $TODO == *"Uninstall Homura"* ]]; then
