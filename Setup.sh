603e9f40955d0b0c0b394ccf577b51aeb5226bad
Alexander Vereeken <37543796+Alexander88207@users.noreply.github.com>
2019-09-11 14:24:46 +0200

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -5,9 +5,10 @@ TODO=$(zenity --list --radiolist --height=50 --width 300 --title="$NAME $VER Ins
 
 if [[ $TODO == *"Install Homura"* ]]; then
 mkdir /home/$USER/.local/share/$NAME
+mkdir /home/$USER/.local/share/$NAME/Data
 cd /home/$USER/.local/share/$NAME
 curl -O https://vignette.wikia.nocookie.net/madoka/images/7/72/Homura_magical_outfit_1.png/revision/latest/top-crop/width/320/height/320?cb=20160821012353
-mv 320?cb=20160821012353 $NAME.png
+mv 320?cb=20160821012353 /home/$USER/.local/share/$NAME/Data/$NAME.png
 curl -O -L https://github.com/Alexander88207/$NAME/archive/$VER.tar.gz
 tar -xf $VER.tar.gz
 mv /home/$USER/.local/share/$NAME/$NAME-$VER/$NAME.sh /home/$USER/.local/share/$NAME/$NAME.sh
@@ -17,7 +18,7 @@ mkdir -p /home/$USER/.local/share/applications
 echo "[Desktop Entry]
 Comment=
 Exec=bash /home/$USER/.local/share/$NAME/$NAME.sh
-Icon=/home/$USER/.local/share/Homura/Homura.png
+Icon=/home/$USER/.local/share/Homura/Data/Homura.png
 Categories=Game;
 Name=Homura
 StartupNotify=false
@@ -36,6 +37,7 @@ UH=$(zenity --question --width 300 --title "Homura - Uninstallation - Warning" -
 if [[ $UH == *"1"* ]]; then 
 notify-send "Thats a good decesion :)"
 fi
+
 if [[ $UH == *"0"* ]]; then 
 rm /home/$USER/.local/share/applications/$NAME.desktop
 rm /home/$USER/Desktop/$NAME.desktop
