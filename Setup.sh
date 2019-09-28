c808876f9893ef0e838cd944df92c4b489b1df97
Alexander Vereeken <37543796+Alexander88207@users.noreply.github.com>
2019-09-28 17:47:02 +0200

    Paths have been simplified and notification improved

--- Setup.sh
+++ Setup.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 NAME=Homura
 VER=2.0
+NDIR=/home/$USER/.local/share/$NAME
+DDIR=$NDIR/Data
+GITACC=Alexander88207
 TODO=$(zenity --list --radiolist --height=50 --width 300 --title="$NAME $VER Installer" --text "What do you want to do?" --hide-header --column "$NAME" --column "Item" FALSE "Install Homura" FALSE "Uninstall Homura")
 
 if [[ $TODO == *"Install Homura"* ]]; then
-mkdir /home/$USER/.local/share/$NAME
-mkdir /home/$USER/.local/share/$NAME/Data
-cd /home/$USER/.local/share/$NAME
+mkdir $NDIR
+mkdir $DDIR
+cd $NDIR
 curl -O https://vignette.wikia.nocookie.net/madoka/images/7/72/Homura_magical_outfit_1.png/revision/latest/top-crop/width/320/height/320?cb=20160821012353
-mv 320?cb=20160821012353 /home/$USER/.local/share/$NAME/Data/$NAME.png
-curl -O -L https://github.com/Alexander88207/$NAME/archive/$VER.tar.gz
+mv 320?cb=20160821012353 $DDIR/$NAME.png
+curl -O -L https://github.com/$GITACC/$NAME/archive/$VER.tar.gz
 tar -xf $VER.tar.gz
-mv /home/$USER/.local/share/$NAME/$NAME-$VER/$NAME.sh /home/$USER/.local/share/$NAME/$NAME.sh
-rm /home/$USER/.local/share/$NAME/$VER.tar.gz
-rm -d -r /home/$USER/.local/share/$NAME/$NAME-$VER
+mv $NDIR/$NAME-$VER/$NAME.sh $NDIR/$NAME.sh
+rm $NDIR/$VER.tar.gz
+rm -d -r $NDIR/$NAME-$VER
 mkdir -p /home/$USER/.local/share/applications
 echo "[Desktop Entry]
 Comment=
-Exec=bash /home/$USER/.local/share/$NAME/$NAME.sh
-Icon=/home/$USER/.local/share/$NAME/Data/$NAME.png
+Exec=bash $NDIR/$NAME.sh
+Icon=$DDIR/$NAME.png
 Categories=Game;
 Name=$NAME
 StartupNotify=false
@@ -30,6 +33,7 @@ X-DBUS-StartupType=
 X-KDE-SubstituteUID=false
 X-KDE-Username=
 " >> /home/$USER/.local/share/applications/$NAME.desktop
+notify-send "The installation is done, have fun!"
 fi
 
 if [[ $TODO == *"Uninstall Homura"* ]]; then
@@ -41,7 +45,7 @@ fi
 if [[ $UH == *"0"* ]]; then 
 rm /home/$USER/.local/share/applications/$NAME.desktop
 rm /home/$USER/Desktop/$NAME.desktop
-rm -d -r /home/$USER/.local/share/$NAME
+rm -d -r $NDIR
+notify-send -i face-sad "The uninstallation is done, sad to see you go."
 fi
 fi
-notify-send Done!
